0ae2264520402a44a25efaf3b4c3ecbabdaedb31
jun.audis5 <jun.audis5@gmail.com>
2025-01-25 23:05:19 +0800

    upadte code

--- caed-airflow/dags/logic/crawler/crawler_104.py
+++ caed-airflow/dags/logic/crawler/crawler_104.py
@@ -1,7 +1,16 @@
-import time, requests
+"""
+Author: PC
+    目的: 爬蟲 104 人力資源網職缺訊息
+    功能:
+        - 最新職缺: 社群行銷 / 軟體工程 / 金融保險 / 業務銷售 / 專案管理
+            - https://www.104.com.tw/jobs/main/newestjob/?jobsource=index_hot_c&utm_medium=cweb_keyword&utm_source=104&tab=job_1
+
+"""
+import time, json, requests
 
 def crawler():
-    print(text)
+    url = 'https://www.104.com.tw/jobs/main/newestjob/?jobsource=index_hot_c&utm_medium=cweb_keyword&utm_source=104&tab=job_1'
+    print(url)
 
 
 
